6825de740eae95c3e4299c8d58089fcab7a6468e
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-02-23 16:46:57 +0900

    Add tweet function

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,24 @@
 class TweetsController < ApplicationController
+  def new
+    @tweet = Tweet.new
+  end
+
   def index
+    @tweets =Tweet.order("created_at DESC")
+  end
+
+  def create
+    @tweet = Tweet.create(image: tweet_params[:image],text: tweet_params[:text])
+    if @tweet.save
+      redirect_to "/"
+    else
+      flash[:danger] = '文字を入力してください'
+      render "new"
+    end
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:image, :text)
   end
 end
