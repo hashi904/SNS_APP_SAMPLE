dd4fdc955d8903e1fcd0f68261371cb51e94c508
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-02-24 10:47:28 +0900

    Edit tweet controller
    Tweet 削除機能の追加
    createのflash messageの編集,Userテーブルへの外部キーの保存を追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,13 +8,19 @@ class TweetsController < ApplicationController
   end
 
   def create
-    @tweet = Tweet.create(image: tweet_params[:image],text: tweet_params[:text])
+    @tweet = Tweet.create(image: tweet_params[:image],text: tweet_params[:text], user_id: current_user.id)
     if @tweet.save
       redirect_to "/"
     else
-      flash[:danger] = '文字を入力してください'
-      render "new"
+      flash.now[:danger] = '文字を入力してください'
+      render :new
     end
+  end
+
+  def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy if tweet.user_id == current_user.id
+    redirect_to "/"
   end
 
   private
