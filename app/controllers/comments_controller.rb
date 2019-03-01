9c2e285ff1489473000a7506b3736c1092d8f56e
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-03-01 21:17:13 +0900

    Create comment controller
    コメントコントローラーを作成し、createアクションとdeleteアクションを追加した。

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,24 @@
 class CommentsController < ApplicationController
+  before_action :set_tweet
+  def create
+    @comment = @tweet.comments.new(comment_params)
+    if @comment.save
+      redirect_to tweet_path(params[:tweet_id])
+    else
+      redirect_to tweet_path(params[:tweet_id]), notice: 'メッセージを入力してください'
+    end
+  end
+  def destroy
+    comment = @tweet.comments.find(params[:id])
+    comment.destroy if comment.user_id == current_user.id
+    redirect_to tweet_path(params[:tweet_id])
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment).merge(user_id: current_user.id)
+  end
+  def set_tweet
+    @tweet = Tweet.find(params[:tweet_id])
+  end
 end
