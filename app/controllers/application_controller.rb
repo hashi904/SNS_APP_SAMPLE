8262a59c3d5f61639c4543323d21274179f91e36
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-03-02 20:08:13 +0900

    Add application controller
    アプリケーションコントローラにprofileの記述を追加した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :username, :image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :username, :image, :profile])
   end
 end
