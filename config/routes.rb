f1530e399c1fcbe37359036e75861e71fdcb44bc
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-03-02 16:02:30 +0900

    Setting routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'likes/create'
+
+  get 'likes/destroy'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root  'tweets#index'
   resources :tweets do
     resources :comments, only: [:create, :destroy]
   end
+  resources :likes, only: [:create, :destroy]
 end
