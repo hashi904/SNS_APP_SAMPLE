79e648689e821cf7c7902f7fe956c9260b8e815d
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-03-04 17:24:27 +0900

    Create likes controller and setting routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
-
   devise_for :users
   resources :users, only: [:index, :show]
+  post "likes/:tweet_id/create" , to: 'likes#create'
+  delete 'likes/:tweet_id/destroy' , to: 'likes#destroy'
   root  'tweets#index'
   resources :tweets do
     resources :comments, only: [:create, :destroy]
