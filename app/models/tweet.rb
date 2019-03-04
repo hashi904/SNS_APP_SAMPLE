e16d887bf005b7d2e34450479914e4d4f898bf49
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-03-04 17:23:51 +0900

    各種アソシエーションの設定

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,11 @@
 class Tweet < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   mount_uploader :image, ImageUploader
   validates :text, presence: true
+
+  def like_user(user_id)
+   likes.find_by(user_id: user_id)
+  end
 end
