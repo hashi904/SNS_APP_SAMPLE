d318eae93c1dd052744005c85a89de5b61218a33
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-03-06 20:44:30 +0900

    Usersモデルにアソシエーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :tweets, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   mount_uploader :image, ImageUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
