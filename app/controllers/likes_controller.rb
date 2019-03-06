4d0b27dfdc33663d1208953adbb834c700507244
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-03-06 20:51:44 +0900

    Add likes_controller
    非同期通信させるためにlikesコントローラにidを記載

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,18 @@
 class LikesController < ApplicationController
   def create
-    @like = Like.create(user_id: current_user.id, tweet_id: params[:tweet_id])
-    redirect_to request.referrer || root_url
+    like = Like.create(user_id: current_user.id, tweet_id: params[:tweet_id])
+    @tweet = like.tweet
+    @id_name = "#like-link-#{@tweet.id}"
+    @id_heart = "#favorited-icon-#{@tweet.id}"
+    # redirect_to request.referrer || root_url
   end
 
   def destroy
     like = Like.find_by(user_id: current_user.id, tweet_id: params[:tweet_id])
+    @tweet = like.tweet
+    @id_name = "#like-link-#{@tweet.id}"
+    @id_heart = "#favorited-icon-#{@tweet.id}"
     like.destroy
-    redirect_to request.referrer || root_url
+    # redirect_to request.referrer || root_url
   end
 end
