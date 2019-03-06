d7c24e81a9ba98d0cd260cf97fa85402d7c17ad7
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-03-06 20:50:35 +0900

    Prefixの追加
    Link_toに記載するリンクを明示的に示すためにroutesに記載を追加した

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:index, :show]
-  post "likes/:tweet_id/create" , to: 'likes#create'
-  delete 'likes/:tweet_id/destroy' , to: 'likes#destroy'
+  post "likes/:tweet_id/create" , to: 'likes#create', as: 'likes_create'
+  delete 'likes/:tweet_id/destroy' , to: 'likes#destroy', as: 'likes_destroy'
   root  'tweets#index'
   resources :tweets do
     resources :comments, only: [:create, :destroy]
