46a52f9111b7d26c0f98615c5c01710ca493fbb5
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-03-11 14:17:00 +0900

    Add controller spec

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe CommentsController, type: :controller do
+  before do
+    @user = FactoryBot.create(:user, id: 1, password: 'hogehoge', email: 'hoge@hoge', nickname: 'hogehoge', username: '@hogehoge')
+    @tweet = FactoryBot.create(:tweet, id: 1, text: 'hoge', user_id: 1)
+    @comment = FactoryBot.create(:comment, id: 1, comment: 'hoge', user_id: 1, tweet_id: 1)
+  end
+
+  describe '#create' do
+    before do
+      sign_in @user
+    end
+
+    # it 'create comment' do
+    #   comment_params = FactoryBot.attributes_for(:comment, {comment: 'hoge', tweet_id: 1})
+    #   expect{
+    #     post :create, params: {tweet:
+    #         {comment: comment_params }
+    #     }
+    #   }.to change(@tweet.comments, :count).by(1)
+    end
+  end
+end
+
