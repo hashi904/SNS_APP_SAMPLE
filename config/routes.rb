b6e7b3873fb77617c558e911720efb97010c6fd9
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-03-11 20:19:53 +0900

    Refactoring routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+  root  'tweets#index'
+
   resources :users, only: [:index, :show] do
     resource :relationships, only: [:create, :destroy]
     get :follows, on: :member
     get :followers, on: :member
   end
+
   post "likes/:tweet_id/create" , to: 'likes#create', as: 'likes_create'
   delete 'likes/:tweet_id/destroy' , to: 'likes#destroy', as: 'likes_destroy'
-  root  'tweets#index'
+
   get 'tweets/search' , to: 'tweets#search', as: 'search'
   resources :tweets do
     resources :comments, only: [:create, :destroy]
