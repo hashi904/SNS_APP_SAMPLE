ddbd97b238ee4618e333d9ef2857911023c3997b
mr904i <hashimoto_keisuke@hashimotokeiyuunoMacBook-puro.local>
2019-03-12 08:38:54 +0900

    ユーザー更新項目の追加
    application_controllerに更新項目を追加した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :username, :image, :profile])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname, :username, :image, :profile])
   end
 end
